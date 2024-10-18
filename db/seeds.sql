3606d2615fe054431ac9c55bf1774566de650c1b
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-17 19:55:12 -0500

    updated employee names

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,11 +16,11 @@ VALUES ('Sales Lead', 1, 100000),
        ('Lawyer', 4, 190000);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, null),
-       ('Mike', 'Chan', 2, 1),
-       ('Ashley', 'Rodriguez', 3, null),
-       ('Kevin', 'Tupik', 4, 3),
-       ('Kunal', 'Singh', 5, null),
-       ('Malia', 'Brown', 6, 5),
-       ('Sarah', 'Lourd', 7, null),
-       ('Tom', 'Allen', 8, 7);
+VALUES ('John', 'Pork', 1, null),
+       ('Bridgett', 'Johnson', 2, 1),
+       ('Ashley', 'Stranded', 3, null),
+       ('Jorge', 'Harrison', 4, 3),
+       ('Victor', 'Vega', 5, null),
+       ('Peter', 'Gabriel', 6, 5),
+       ('Gary', 'Tang', 7, null),
+       ('Jason', 'Parse', 8, 7);
